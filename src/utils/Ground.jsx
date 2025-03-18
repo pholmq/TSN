9118e0767299150cad6d57fd19c50b737df47e8a
pholmq <pholmq@gmail.com>
2025-03-18 16:22:24 +0100

    more plancam

--- src/utils/Ground.jsx
+++ src/utils/Ground.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { useTexture } from "@react-three/drei";
 import { Vector3 } from "three";
+import { useSettingsStore } from "../store.js"
 
-export default function Ground({ data, position = [0, 0, 0] }) {
+export default function Ground({ planet, position = [0, 0, 0] }) {
+  const getSetting = useSettingsStore((state) => state.getSetting);
+  const s = getSetting(planet)
+console.log(s)
+
   return (
     <group position={position} rotation={[-Math.PI / 2, 0, 0]}>
-      {data.name === "Earth" ? (
-        <Earth data />
+      {s.name === "Earth" ? (
+        <Earth s />
       ) : (
         <mesh>
-          <circleGeometry args={[data.size, 64]} />
-          <meshStandardMaterial color={data.color} opacity={0.5} transparent />
+          <circleGeometry args={[s.size, 64]} />
+          <meshStandardMaterial color={s.color} opacity={0.5} transparent />
         </mesh>
       )}
     </group>
   );
 }
-function Earth({ data }) {
+function Earth({ s }) {
   const [texture] = useTexture(["/textures/grass.png"]);
   return (
     <mesh>
-      <circleGeometry args={[data.size, 64]} />
+      <circleGeometry args={[s.size, 64]} />
       <meshStandardMaterial map={texture} />
     </mesh>
   );
