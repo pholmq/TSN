94101d238fa17230ea69941c433ea9e712c92ec0
pholmq <pholmq@gmail.com>
2025-04-02 11:44:23 +0200

    plancam ui work

--- src/components/PlanetCamera/PlanetCameraUI.jsx
+++ src/components/PlanetCamera/PlanetCameraUI.jsx
@@ -2,10 +2,18 @@ import { useEffect, useMemo } from "react";
 import { useControls, useCreateStore, Leva, folder } from "leva";
 import { useStore, useSettingsStore, usePosStore } from "../../store";
 
+import {
+  azEl2RaDec,
+  rad2lat,
+  rad2lon,
+  radiansToAzimuth,
+} from "../../utils/celestial-functions";
+
 const PlanetCameraUI = () => {
   const showPositions = useStore((s) => s.showPositions);
   const planetCamera = useStore((s) => s.planetCamera);
   const planetCameraDirection = useStore((s) => s.planetCameraDirection);
+  const { latRotationx, longRotationy, height } = planetCameraDirection;
   const positions = usePosStore((s) => s.positions);
   const { settings } = useSettingsStore();
 
@@ -13,14 +21,32 @@ const PlanetCameraUI = () => {
   const plancamUIStore = useCreateStore();
   const [, set] = useControls(
     () => ({
-      Latitude: { value: "10'30#" },
+      Latitude: {
+        value: rad2lat(latRotationx),
+        hint: "Camera latitude in decimal degrees",
+        onChange: () => {},
+      },
+      Longitude: {
+        value: rad2lon(longRotationy),
+        hint: "Camera longitude in decimal degrees",
+        onChange: () => {},
+      },
+      Height: {
+        value: height * 100,
+        hint: "Camera height over planet center",
+        onChange: () => {},
+      },
     }),
     { store: plancamUIStore }
   );
 
-  // Update Leva controls when camera pos change
+  // Update Leva controls when camera pos changes
   useEffect(() => {
-    console.log({...planetCameraDirection});
+    // console.log(planetCameraDirection.height);
+
+    set({ Latitude: rad2lat(latRotationx) });
+    set({ Longitude: rad2lon(longRotationy) });
+    set({ Height: height * 100 });
   }, [planetCameraDirection]);
 
   return (
