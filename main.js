e604511003b6296d6bda3a362ff470a43dc3af60
pholmq <pholmq@gmail.com>
2025-12-17 11:30:11 +0100

    Enable automatic publishing to GitHub Releases

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
   }
 
   //open console (for debugging only)
-  win.webContents.openDevTools();
+ // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
