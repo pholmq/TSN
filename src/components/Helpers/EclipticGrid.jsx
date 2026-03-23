6b64d9f992467c2f780891adf2ebcff7214db23e
pholmq <pholmq@gmail.com>
2026-03-23 13:48:56 +0100

    ecl grid fix

--- src/components/Helpers/EclipticGrid.jsx
+++ src/components/Helpers/EclipticGrid.jsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import * as THREE from "three";
-import { Text } from "@react-three/drei";
+import { Text3D, Center } from "@react-three/drei";
 import { useStore } from "../../store";
 
 export default function EclipticGrid() {
@@ -23,14 +23,22 @@ export default function EclipticGrid() {
     const material = new THREE.LineBasicMaterial({ color: "#008800" });
     const greenLines = new THREE.LineSegments(geometry, material);
 
+    // PERFORMANCE FIX: Prevent CPU raycast checks on the grid lines
+    greenLines.raycast = () => null;
+
     group.add(greenLines);
 
     return group;
-  }, []); // eclipticGridSize removed from dependency array as it's not used inside useMemo
+  }, []);
 
   if (!eclipticGrid) return null;
 
   const size = (eclipticGridSize * hScale) / 100;
+
+  // Use the local font file you already have in your public/fonts folder
+  const fontPath = process.env.PUBLIC_URL
+    ? process.env.PUBLIC_URL + "/fonts/Cambria_Regular.json"
+    : "/fonts/Cambria_Regular.json";
 
   return (
     <group position={[0, 0, 0]} scale={[size, size, size]}>
@@ -39,52 +47,36 @@ export default function EclipticGrid() {
       {/* Seasonal Markers */}
 
       {/* Vernal Equinox (March) - Right (+X) */}
-      <Text
-        position={[1, 0.03, 0]}
-        rotation={[0, -Math.PI / 2, 0]}
-        fontSize={0.05}
-        color="#FF4500"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Summer Solstice
-      </Text>
+      <Center position={[1, 0.03, 0]} rotation={[0, -Math.PI / 2, 0]}>
+        <Text3D font={fontPath} size={0.05} height={0.005}>
+          <meshBasicMaterial color="#FF4500" />
+          {"Summer Solstice"}
+        </Text3D>
+      </Center>
 
       {/* Autumnal Equinox (September) - Left (-X) */}
-      <Text
-        position={[-1, 0.03, 0]}
-        rotation={[0, Math.PI / 2, 0]}
-        fontSize={0.05}
-        color="#FF4500"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Winter Solstice
-      </Text>
+      <Center position={[-1, 0.03, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <Text3D font={fontPath} size={0.05} height={0.005}>
+          <meshBasicMaterial color="#FF4500" />
+          {"Winter Solstice"}
+        </Text3D>
+      </Center>
 
       {/* Summer Solstice (June) - Front (+Z) - Facing correct direction */}
-      <Text
-        position={[0, 0.03, 1]}
-        rotation={[0, Math.PI, 0]} // Flipped 180 degrees
-        fontSize={0.05}
-        color="#FF8C00"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Vernal Equinox
-      </Text>
+      <Center position={[0, 0.03, 1]} rotation={[0, Math.PI, 0]}>
+        <Text3D font={fontPath} size={0.05} height={0.005}>
+          <meshBasicMaterial color="#FF8C00" />
+          {"Vernal Equinox"}
+        </Text3D>
+      </Center>
 
       {/* Winter Solstice (December) - Back (-Z) - Facing correct direction */}
-      <Text
-        position={[0, 0.03, -1]}
-        rotation={[0, 0, 0]} // Reset to 0 rotation
-        fontSize={0.05}
-        color="#FF8C00"
-        anchorX="center"
-        anchorY="middle"
-      >
-        Autumnal Equinox
-      </Text>
+      <Center position={[0, 0.03, -1]} rotation={[0, 0, 0]}>
+        <Text3D font={fontPath} size={0.05} height={0.005}>
+          <meshBasicMaterial color="#FF8C00" />
+          {"Autumnal Equinox"}
+        </Text3D>
+      </Center>
     </group>
   );
 }
